dcce107cf555096e511066b8c86bc53cb1e2b9c7
Brayden <94504031+Brayden-L@users.noreply.github.com>
2021-12-16 16:55:30 -0500

    Deleted everything and regenerated the project because IntelliJ wouldn't let me convert to Java 8

--- core/src/com/mygdx/game/ParticleSimulatorClass.java
+++ core/src/com/mygdx/game/ParticleSimulatorClass.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class GDXgame extends ApplicationAdapter {
+public class ParticleSimulatorClass extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	

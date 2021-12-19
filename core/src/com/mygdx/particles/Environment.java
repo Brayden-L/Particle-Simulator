dd64a208fd5e03cac549864bdfa2af60e7245f9b
Brayden <94504031+Brayden-L@users.noreply.github.com>
2021-12-18 23:23:06 -0500

    TOML base functionality and codebase refactoring

--- core/src/com/mygdx/particles/Environment.java
+++
@@ -1,8 +0,0 @@
-package com.mygdx.particles;
-
-public class Environment {
-    int[]       dimensions;
-    float       fGrav;  // https://www.britannica.com/science/gravitational-constant
-    String[]    particles;
-
-}
---
+++ core/src/com/mygdx/phys/Environment.java
@@ -0,0 +1,8 @@
+package com.mygdx.phys;
+
+public class Environment {
+    int[]       dimensions;
+//    float       fGrav;  // https://www.britannica.com/science/gravitational-constant
+    String[]    particles;
+
+}

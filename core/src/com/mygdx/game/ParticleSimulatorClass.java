b30a11f491d8557d30855310e56c3ef3bc6fb2b1
Brayden <94504031+Brayden-L@users.noreply.github.com>
2021-12-24 00:28:21 -0500

    i learned how to read and got the toml loader to not die

--- core/src/com/mygdx/game/ParticleSimulatorClass.java
+++ core/src/com/mygdx/game/ParticleSimulatorClass.java
@@ -1,3 +1,7 @@
+/*
+    Copyright (C) 2021  Brayden L.
+*/
+
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
@@ -5,10 +9,62 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import com.moandjiezana.toml.Toml;
+import com.mygdx.phys.Environment;
+import com.mygdx.phys.physParticle;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class ParticleSimulatorClass extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+	//    public String[] particleFactory(Toml tomlData) {
+//        ArrayList particleArray = new ArrayList<>;
+
+	/* Particle generator: */
+/*        assert false;
+        for (String item : tomlData) {
+            new Thread(
+                    new physParticle(
+                            "",
+                            "",
+                            a,
+                            b
+                    )
+            ).start();
+        }
+
+        return particleArray;
+    }
+*/
+	public ParticleSimulatorClass(/*String arg*/) {
+
+		// https://www.w3schools.com/java/java_files_read.asp
+		// https://github.com/mwanji/toml4j
+/*
+		File tomlFile;
+		Toml toml = null;
+		try {
+			tomlFile = new File(arg);
+			toml = new Toml().read(tomlFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+*/
+		File tomlFile = new File("/home/fruitcake/Projects/Particle-Simulator/core/src/com/mygdx/example.toml");
+		Map<String, Object> tomlMap = new Toml().read(tomlFile).toMap();
+		//Toml toml = new Toml().read(tomlFile);
+
+		System.out.println(tomlMap);
+//        Environment env = new Environment();
+
+	}
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -28,4 +84,9 @@ public class ParticleSimulatorClass extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 	}
+
+	public void main() {
+
+	}
+
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,59 +7,20 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.phys.*;
-import com.moandjiezana.toml.Toml;
-import com.mygdx.phys.Environment;
-import com.mygdx.phys.physParticle;
-import java.util.ArrayList;
-import java.io.File;
-import java.util.Arrays;
+import com.mygdx.game.ParticleSimulatorClass;
 
 public class DesktopLauncher  {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Particle Simulator";
 		config.width = 1800;
 		config.height = 900;
 
-		final File tomlSource;
-
-		File tomlSource1;
-
 		try {
-			tomlSource1 = new File(Arrays.toString(args));
-		} catch (Exception e) {
-			tomlSource1 = null;
+//			new LwjglApplication(new ParticleSimulatorClass(args[0]), config);
+			new LwjglApplication(new ParticleSimulatorClass(), config);
+		} catch (ArrayIndexOutOfBoundsException e) {
 			e.printStackTrace();
 		}
-
-		tomlSource = tomlSource1;
-		Toml tomlData = new Toml().read(tomlSource);
-
-		System.out.println(tomlData);
-// 	WE DON'T TALK ABOUT THIS......
-		//    public String[] particleFactory(Toml tomlData) {
-//        ArrayList particleArray = new ArrayList<>;
-
-		/* Particle generator: */
-/*        assert false;
-        for (String item : tomlData) {
-            new Thread(
-                    new physParticle(
-                            "",
-                            "",
-                            a,
-                            b
-                    )
-            ).start();
-        }
-
-        return particleArray;
-    }
-*/
-
-//        Environment env = new Environment();
-
-		new LwjglApplication(new DesktopLauncher(), config);
 	}
 }
